3e362bb4ad84da9295e2cbe2ed571bdfa7a6c9b6
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-04-19 23:23:43 -0400

    MVP

--- client/src/App.test2.js
+++ client/src/App.test2.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import * as rtl from '@testing-library/react' 
+import * as rtl from '@testing-library/react' ;
 import '@testing-library/jest-dom/extend-expect';
+import App from './App'
 
 
 
